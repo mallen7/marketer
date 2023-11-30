b2491a68c66fd3625074f249eabfb57ce684d109
Allen, Scott <MAllen@tcsg.edu>
2023-11-30 12:35:52 -0500

    changed paths

--- custom/domain-cleaner.sh
+++ custom/domain-cleaner.sh
@@ -13,8 +13,8 @@ fi
 export filename=$1
 
 # Replace these with your actual file paths
-export input_file="./processing/extracted-domains/dirty/${filename}"
-export output_file="./processing/extracted-domains/clean/${filename}"
+export input_file="processing/extracted-domains/dirty/${filename}"
+export output_file="processing/extracted-domains/clean/${filename}"
 
 # Regular expression for domain validation
 regex='^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][-a-zA-Z0-9]{0,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$'
